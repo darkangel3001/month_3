adc61a0972d156ee3a0be0c315d8dacbc39f6b63
darkangel3001 <dizum34@gmail.com>
2024-11-07 17:18:05 +0600

    Fixing random file

--- handlers/random.py
+++ handlers/random.py
@@ -7,14 +7,14 @@ random_router = Router()
 
 # Список рецептов
 recipes = [
-    {"name": "Оливье", "image": "images/olivier.jpeg", "caption": "Рецепт: для Оливье нужно: картошку, горох, яйца, огурцы,колбасу или мясо ."},
-    {"name": "Лагман", "image": "images/lagman.jpeg", "caption": "Рецепт: для Лагмана нужно: лапшу, мясо, зеленый и красные перцы, лук, и некоторые овощи по вкусу."},
-    {"name": "Плов", "image": "images/plov.jpeg", "caption": "Рецепт: для плова нужно: рис, мясо, морковка ."},
+    {"name": "Оливье", "image": "image/olivier.jpeg", "caption": "Рецепт: для Оливье нужно: картошку, горох, яйца, огурцы,колбасу или мясо ."},
+    {"name": "Лагман", "image": "image/lagman.jpeg", "caption": "Рецепт: для Лагмана нужно: лапшу, мясо, зеленый и красные перцы, лук, и некоторые овощи по вкусу."},
+    {"name": "Плов", "image": "image/plov.jpeg", "caption": "Рецепт: для плова нужно: рис, мясо, морковка ."},
 ]
 
 @random_router.message(Command("random"))
 async def random_handler(message: types.Message):
     recipe =choice(recipes)
-    photo=FSInputFile(recipe['photo'])
+    photo= FSInputFile(recipe['image'])
     await message.answer_photo(photo, caption=recipe["caption"])
 
